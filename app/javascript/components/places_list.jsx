e96121d5336594ad473e3e9c83d9a2fcf85a01a2
zeeshan rafi <zeeshan.rafi@devsinc.com>
2023-05-21 23:18:38 +0500

    Add tailswind styles

--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -4,8 +4,12 @@ import ReactDOM from "react-dom/client";
 
 
 const PlacesList = () => {
+  const HeaderStyle = "text-sm font-medium text-gray-900 px-6 py-4 text-left";
+  const DataStyle = "text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap";
+
   const [loading, setLoading] = useState(true);
   const [places, setPlaces] = useState([]);
+
 
   useEffect(()=>{
     const apiEndPoint = "api/places"
@@ -23,33 +27,39 @@ const PlacesList = () => {
         loading ?
           <div>Loading content...</div>
         :
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>City</th>
-                <th>Recent Upload Speed</th>
-                <th>Recent Upload Speed Units</th>
-                <th>No, of measurements</th>
-              </tr>
-            </thead>
-            <tbody>
-                {
-                  places.map((place, index) => {
-                    return(
-                      <tr key={index}>
-                        <td>{place.name}</td>
-                        <td>{place.city}</td>
-                        <td>{place.most_recent_download_speed}</td>
-                        <td>{place.most_recent_download_units}</td>
-                        <td>{place.number_of_measurements}</td>
-                      </tr>
-                    )
-                  })
-                }
-            </tbody>
-          </table>
+        <div className="flex flex-col">
+          <div className="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
+            <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+              <div className="overflow-hidden">
+                <table className="min-w-full">
+                  <thead className="bg-gray-200 border-b">
+                    <tr>
+                      <th scope="col" className={HeaderStyle}>Name</th>
+                      <th scope="col" className={HeaderStyle}>City</th>
+                      <th scope="col" className={HeaderStyle}>Recent Upload Speed</th>
+                      <th scope="col" className={HeaderStyle}>Recent Upload Speed Units</th>
+                      <th scope="col" className={HeaderStyle}>No, of measurements</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                      {
+                        places.map((place, index) => {
+                          return(
+                            <tr key={index} className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                              <td className={DataStyle}>{place.name}</td>
+                              <td className={DataStyle}>{place.city}</td>
+                              <td className={DataStyle}>{place.most_recent_download_speed}</td>
+                              <td className={DataStyle}>{place.most_recent_download_units}</td>
+                              <td className={DataStyle}>{place.number_of_measurements}</td>
+                            </tr>
+                          )
+                        })
+                      }
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
       }
     </>
