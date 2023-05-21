32850726bff2ca2f07394e96e8a5a5fff5add13c
zeeshan rafi <zeeshan.rafi@devsinc.com>
2023-05-21 18:29:51 +0500

    Added API for places

---
+++ app/controllers/api/places_controller.rb
@@ -0,0 +1,30 @@
+module Api
+  class PlacesController < ApplicationController
+    def index
+      places = Place.all.map do |place|
+        {
+          name: place.name,
+          city: place.city,
+          most_recent_download_speed: most_recent_download_speed(place),
+          most_recent_download_units: most_recent_download_units(place),
+          number_of_measurements: number_of_measurements(place)
+        }
+      end
+
+      render(json: {places: places})
+    end
+
+    def most_recent_download_speed(place)
+      place.internet_speeds.order("created_at").last
+    end
+
+    def most_recent_download_units(place)
+      place.internet_speeds.order("created_at").last
+    end
+
+    def number_of_measurements(place)
+      place.internet_speeds.count
+    end
+
+  end
+end
--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -1,16 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 
-class PlacesList extends React.Component {
-  render () {
-    return(
-      <div>
-        <h1>Places List</h1>
-      </div>
-    )
-  }
-}
 
+
+const PlacesList = () => {
+  const [loading, setLoading] = useState(true);
+  const [places, setPlaces] = useState([]);
+
+  useEffect(()=>{
+    const apiEndPoint = "api/places"
+    fetch(apiEndPoint)
+    .then( response => response.json() )
+    .then( data => {
+      setPlaces(data["places"])
+      setLoading(false)
+      })
+  }, [])
+
+  return(
+    <>
+      {
+        loading ?
+          <div>Loading content...</div>
+        :
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>City</th>
+                <th>Recent Upload Speed</th>
+                <th>Recent Upload Speed Units</th>
+                <th>No, of measurements</th>
+              </tr>
+            </thead>
+            <tbody>
+                {
+                  places.map((place, index) => {
+                    return(
+                      <tr key={index}>
+                        <td>{place.name}</td>
+                        <td>{place.city}</td>
+                        <td>{place.most_recent_download_speed}</td>
+                        <td>{place.most_recent_download_units}</td>
+                        <td>{place.number_of_measurements}</td>
+                      </tr>
+                    )
+                  })
+                }
+            </tbody>
+          </table>
+        </div>
+      }
+    </>
+  )
+}
 
 const placesList = ReactDOM.createRoot(document.getElementById('places-list-container'));
 placesList.render(< PlacesList />)
