695b872333f6b20ccf228aa688ae5d0e8732f7d5
zeeshan rafi <zeeshan.rafi@devsinc.com>
2023-05-23 15:22:08 +0500

    Added log speed form

--- app/javascript/components/internet_speeds_new.jsx
+++ app/javascript/components/internet_speeds_new.jsx
@@ -2,8 +2,36 @@ import React, { useEffect, useState } from "react";
 
 export default InternetSpeedNew = () => {
     return(
-      <div>
-        Here is internet speed page
-      </div>
+      <>
+            <div>
+              <h1>Log Internet Speed</h1>
+            </div>
+        		<form className="px-8 pt-6 pb-8 mb-4 bg-white rounded">
+							<div className="mb-4">
+								<div className="mb-4 md:ml-2">
+									<label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="firstName">
+										First Name
+									</label>
+									<input
+										className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="firstName"
+										type="text"
+										placeholder="First Name"
+									/>
+								</div>
+								<div className="md:ml-2 mb-4">
+									<label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="lastName">
+										Last Name
+									</label>
+									<input
+										className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="lastName"
+										type="text"
+										placeholder="Last Name"
+									/>
+								</div>
+							</div>
+						</form>
+      </>
     )
 }
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  get "api/places", to: "api/places#index"
+  get "*path", to: "places#index"
   root "places#index"
 
-  get "api/places", to: "api/places#index"
-  get "/new-internet-speed", to: "internet_speeds#new"
 end
