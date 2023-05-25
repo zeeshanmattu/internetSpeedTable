39d498b1fea69965fff69d753ba077d03988e684
zeeshan rafi <zeeshan.rafi@devsinc.com>
2023-05-25 18:59:16 +0500

    added speed tester

--- app/javascript/components/internet_speeds_new.jsx
+++ app/javascript/components/internet_speeds_new.jsx
@@ -1,6 +1,21 @@
 import React, { useEffect, useState } from "react";
+import { ReactInternetSpeedMeter } from 'react-internet-meter'
 
 export default InternetSpeedNew = () => {
+    const [testInProgress, setTestInProgress] = useState(false)
+    const [downloadSpeeds, setDownloadSpeeds] = useState([])
+    const onDownloadSpeedChange = (speed) => {
+      const newDownloadSpeeds = downloadSpeeds.add(speed);
+      if( newDownloadSpeeds.length > 5 ){
+        setTestInProgress(false);
+        console.log(`Test Finished, results are: ${downloadSpeeds}`)
+      }
+      setDownloadSpeeds(newDownloadSpeeds)
+
+    }
+
+
+
     return(
       <>
             <div>
@@ -30,6 +45,36 @@ export default InternetSpeedNew = () => {
 										placeholder="Last Name"
 									/>
 								</div>
+							</div>
+              <div className="mb-6 text-center">
+                { testInProgress &&
+                  <div>
+                    <div>Testing</div>
+                    <ReactInternetSpeedMeter
+                      txtSubHeading="Internet is too slow"
+                      outputType="alert"
+                      customClassName={null}
+                      txtMainHeading="Opps..."
+                      pingInterval={1000} // milliseconds
+                      thresholdUnit='megabyte' // "byte" , "kilobyte", "megabyte"
+                      threshold={0}
+                      imageUrl="https://cdn.speedcheck.org/images/reviews/google-speed-test-mobile.jpg"
+                      downloadSize="156760"  //bytes
+                      callbackFunctionOnNetworkDown={(speed)=>console.log(`Internet speed is down ${speed}`)}
+                      callbackFunctionOnNetworkTest={(speed)=>console.log(`Zeeshan ${speed}`)}
+                      />
+                  </div>
+                }
+                { !testInProgress && downloadSpeeds.length == 0 &&(
+                  <button
+                  className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+                  type="button"
+                  onClick={()=>setTestInProgress(true)}
+                >
+                  Submit
+                </button>
+                )}
+
 							</div>
 						</form>
       </>
